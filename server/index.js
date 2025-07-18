6d5529f0da2a24d9164213a7f7cadd1c8561a9df
creation7654321 <creation.pradhan@gmail.com>
2025-07-18 14:18:20 +0545

    inital setup for server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from 'dotenv';
+import { connectToDb } from "./src/config/db.js";
+
+dotenv.config();
+
+connectToDb();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/',(req,res)=>{
+    console.log("Welcome");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
---
+++ server/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectToDb = async()=>{
+    const uri = process.env.MONGODB_URI;
+    try {
+        await mongoose.connect(uri);
+        console.log("MongoDb has been connected");
+    } catch (error) {
+        console.log("Error to connect to MongoDB",error);
+        process.exit(1);
+    }
+    mongoose.connection.on("error",(error)=>{
+        console.log("Mongodb connection error");
+    })
+}
