c979157207d673d7e45c970c631ac599bda553de
creation7654321 <creation.pradhan@gmail.com>
2025-07-27 16:52:23 +0545

    creating getAllCategory, updateCategory, deleteCategory, singleCategory

--- server/src/controller/Category.js
+++ server/src/controller/Category.js
@@ -1,6 +1,6 @@
 import CategoryModel from "../models/Category.js";
 import slug from 'slugify';
-
+import postModel from "../models/Post.js";
 
 export const createCategoryController = async(req,res)=>{
     try {
@@ -57,8 +57,59 @@ export const getAllCategoryController = async(req,res)=>{
 
 export const updateCategoryController = async(req,res)=>{
     try {
-        
+        const{name} = req.body;
+        const{id} = req.params;
+        const category = await CategoryModel.findByIdAndUpdate(
+            id,
+            {name , slug:slug(name) },
+            {new:true}
+        );
+        return res.status(200).send({
+            success:true,
+            message: "Category updated successflly",
+            category
+        })
     } catch (error) {
-        
+        return res.staus(500).send({
+            success:false,
+            message: "Error while updating category",
+            error
+        })
+    }
+}
+
+export const deleteCategoryController = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        CategoryModel.findByIdAndDelete(id);
+        return res.status(200).send({
+            success: true,
+            message: "Category deleted successfully"
+        })
+    } catch (error) {
+        return res.status(500).send({
+            success:false,
+            messag: "Error while deleteing category",
+            error,
+        })
+    }
+}
+
+export const singleCategory = async(req,res)=>{
+    try {
+        const category = await CategoryModel.findOne({slug:req.params.slug});
+        const post = await postModel.find({category}).populate("category")
+        return res.send(200).send({
+            success:true,
+            message: "category fetched succesfully",
+            category,
+            post
+        })
+    } catch (error) {
+        return res.status(404).send({
+            success:false,
+            messag: "Category not found",
+            error,
+        })
     }
 }
--- server/src/routes/Category.js
+++ server/src/routes/Category.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createCategoryController, getAllCategoryController,updateCategoryController } from "../controller/Category.js";
+import { createCategoryController, getAllCategoryController,updateCategoryController, deleteCategoryController, singleCategory } from "../controller/Category.js";
 import { isAdmin ,requireSignIn} from "../middlewares/Auth.js";
 const app = express.Router();
 
 app.post('/create-category',isAdmin,requireSignIn,createCategoryController);
 app.get('/get-category',isAdmin, requireSignIn,getAllCategoryController);
 app.put('/update-category/:id',isAdmin, requireSignIn, updateCategoryController);
-
+app.delete('/delete-category/:id',isAdmin, requireSignIn,deleteCategoryController);
+app.get('single-category/:slug',singleCategory)
 export default app;
