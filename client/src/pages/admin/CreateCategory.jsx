fa8ff9b34053284032af23b2ecdae5e9ac8e9ef4
creation7654321 <creation.pradhan@gmail.com>
2025-07-28 20:26:02 +0545

    making cretPost form

--- client/src/pages/admin/CreateCategory.jsx
+++ client/src/pages/admin/CreateCategory.jsx
@@ -6,8 +6,18 @@ import { toast } from 'react-toastify';
 function CreateCategory() {
 
   const [category, setCategory] = useState([]);
+  const [categoryName, setCategoryName] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [editId, setEditId] = useState(null);
 
+  const handleDelete = async(id)=>{
+    try {
+      await axios.delete(`${import.meta.env.VITE_BASE_URL}/api/category/delete-category/${id}`)
+      fetchCategories();
+    } catch (error) {
+      console.log("Error deleting category");
+    }
+  }
   // Fetch categories from API
   const fetchCategories = async () => {
     try {
@@ -19,6 +29,21 @@ function CreateCategory() {
       console.error("Error fetching categories:", error);
     }
   };
+
+  const handleEdit = (id, name)=>{
+    setEditId(id);
+    setCategoryName(name);
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    try {
+      
+    } catch (error) {
+      
+    }
+
+  }
 
   useEffect(() => {
     fetchCategories();
@@ -33,44 +58,44 @@ function CreateCategory() {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
 
-    // Validate form fields
-    if (
-      !title ||
-      !hotelLocation ||
-      !description ||
-      !facilities ||
-      !nearArea ||
-      !selectedCategory ||
-      !guest ||
-      !price
-    ) {
-      return toast.error("All fields are required.");
-    }
+  //   // Validate form fields
+  //   if (
+  //     !title ||
+  //     !hotelLocation ||
+  //     !description ||
+  //     !facilities ||
+  //     !nearArea ||
+  //     !selectedCategory ||
+  //     !guest ||
+  //     !price
+  //   ) {
+  //     return toast.error("All fields are required.");
+  //   }
 
-    if (images.length !== 3) {
-      return toast.error("Please upload exactly 3 images.");
-    }
+  //   if (images.length !== 3) {
+  //     return toast.error("Please upload exactly 3 images.");
+  //   }
 
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("hotelLocation", hotelLocation);
-    formData.append("description", description);
-    formData.append("facilities", facilities);
-    formData.append("nearArea", nearArea);
-    formData.append("category", selectedCategory);
-    formData.append("guest", guest);
-    formData.append("isAvailable", isAvailable);
-    formData.append("price", price);
+  //   const formData = new FormData();
+  //   formData.append("title", title);
+  //   formData.append("hotelLocation", hotelLocation);
+  //   formData.append("description", description);
+  //   formData.append("facilities", facilities);
+  //   formData.append("nearArea", nearArea);
+  //   formData.append("category", selectedCategory);
+  //   formData.append("guest", guest);
+  //   formData.append("isAvailable", isAvailable);
+  //   formData.append("price", price);
 
-    images.forEach((file) => {
-      formData.append("images", file);
-    });
+  //   images.forEach((file) => {
+  //     formData.append("images", file);
+  //   });
 
     
-  };
+  // };
   return (
     <div >
      
